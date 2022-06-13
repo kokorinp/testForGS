4b1a97e1a6e207d08f6e9f718604c132b9ff11c6
TARAN <taran123@yandex.ru>
2022-06-13 15:00:41 +0300

    Настройка среды

---
+++ client/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    semi: true,
+    tabWidth: 2,
+    arrowParens: 'always',
+};
